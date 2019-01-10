0a0ecaa4a57061e6fbb62645a1263c851aa64cc0
Saroumoun <bastien.hiel@gmail.com>
2019-01-10 10:50:09 +0100

    added combo

--- lib/00_hello.rb
+++ lib/00_hello.rb
@@ -1,5 +1,19 @@
+
+def ask_first_name
+	puts "Quel est ton prénom ?"
+	user_name = gets.chomp
+	return user_name
+end
+
+
 def say_hello (first_name)
 	puts "Bonjour, #{first_name}!"
 end
 
-say_hello("bastien")
+
+def perform
+	first_name = ask_first_name
+	say_hello(first_name)
+end
+
+perform
